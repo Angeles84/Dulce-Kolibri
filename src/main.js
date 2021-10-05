feea13fddad8ea650552ce90b5bc9a4ec79b4ebd
Angeles <angie.oliva@gmail.com>
2021-10-04 22:39:23 -0300

    borrando la apikey

--- src/main.js
+++ src/main.js
@@ -8,12 +8,7 @@ import Firebase from 'firebase'
 Vue.config.productionTip = false
 
 Firebase.initializeApp({
-  apiKey: "AIzaSyBiAwPCeaR24J-1KYHK9r1FPqy6o-QrLAc",
-  authDomain: "dulce-kolibri.firebaseapp.com",
-  projectId: "dulce-kolibri",
-  storageBucket: "dulce-kolibri.appspot.com",
-  messagingSenderId: "187799089534",
-  appId: "1:187799089534:web:10bbda90178375c40d432f"
+  
 })
 
 new Vue({
