0e60fe61b0bbf0b48234065db242d234ad395d20
Angeles <angie.oliva@gmail.com>
2021-10-06 10:17:38 -0300

    eliminando api

--- src/main.js
+++ src/main.js
@@ -8,16 +8,7 @@ import Firebase from 'firebase'
 Vue.config.productionTip = false
 
 Firebase.initializeApp({
-<<<<<<< HEAD
   
-=======
-  apiKey: "AIzaSyBiAwPCeaR24J-1KYHK9r1FPqy6o-QrLAc",
-  authDomain: "dulce-kolibri.firebaseapp.com",
-  projectId: "dulce-kolibri",
-  storageBucket: "dulce-kolibri.appspot.com",
-  messagingSenderId: "187799089534",
-  appId: "1:187799089534:web:10bbda90178375c40d432f"
->>>>>>> 4c8e7999b376d988f779ab2c59c689a4f3088b47
 })
 
 new Vue({
