4c116057f3f595c5b0dce1cf798cd7abce6d46d3
Luis Arancibia <l.arancibiaf@gmail.com>
2021-10-06 21:17:58 -0300

    replace hardcoded vars with processEnv

--- src/views/Carrito.vue
+++ src/views/Carrito.vue
@@ -24,7 +24,7 @@
 
       <v-row
         class="text-left"
-        v-for="(producto , id) in $store.state.productos"
+        v-for="(producto, id) in $store.state.productos"
         :key="id"
       >
         <v-col cols="12" md="4" class="pl-10 d-flex">
@@ -37,24 +37,54 @@
           </div>
         </v-col>
         <v-col cols="12" md="2">
-          <h4 class="pt-7 font-weight-bold">$ {{producto.precio.toLocaleString("de-DE", {minimumFractionDigits: 0})}}</h4>
+          <h4 class="pt-7 font-weight-bold">
+            $
+            {{
+              producto.precio.toLocaleString("de-DE", {
+                minimumFractionDigits: 0,
+              })
+            }}
+          </h4>
         </v-col>
         <v-col cols="12" md="2" class="pl-8">
           <div class="pt-5">
-            <button class="btn-menos mr-1"
-              @click="$store.dispatch('restarCantidadAlProductoDelCarritoDeCompras' , id)" :disabled="producto.qty === 1"
-            >-</button>
-            <span class="px-1"><b>{{producto.qty}}</b></span>
-            <button class="btn-mas"
-              @click="$store.dispatch('agregarCantidadAlProductoDelCarritoDeCompras' , id)"
-            >+</button>
+            <button
+              class="btn-menos mr-1"
+              @click="
+                $store.dispatch(
+                  'restarCantidadAlProductoDelCarritoDeCompras',
+                  id
+                )
+              "
+              :disabled="producto.qty === 1"
+            >
+              -
+            </button>
+            <span class="px-1"
+              ><b>{{ producto.qty }}</b></span
+            >
+            <button
+              class="btn-mas"
+              @click="
+                $store.dispatch(
+                  'agregarCantidadAlProductoDelCarritoDeCompras',
+                  id
+                )
+              "
+            >
+              +
+            </button>
           </div>
         </v-col>
         <v-col cols="12" md="4" class="pl-14">
           <div class="d-flex justify-space-between pr-5">
             <h4
               class="pt-7 font-weight-bold"
-              v-text="`$ ${producto.precio.toLocaleString('de-DE', {minimumFractionDigits: 0})}`"
+              v-text="
+                `$ ${producto.precio.toLocaleString('de-DE', {
+                  minimumFractionDigits: 0,
+                })}`
+              "
             ></h4>
             <v-btn small plain class="mt-5" @click="eliminarProducto(id)">
               <v-icon color="#c59206">mdi-delete</v-icon>
@@ -66,48 +96,75 @@
     </v-container>
 
     <!--Container pantalla chica-->
-    <v-container
-      class="contenedor-chico px-4"
-    >
+    <v-container class="contenedor-chico px-4">
       <div class="d-flex justify-space-between">
         <h3>Producto</h3>
         <h3>Precio</h3>
       </div>
-      <hr>
+      <hr />
       <v-row
         class=""
-        v-for="(producto , id) in $store.state.productos"
+        v-for="(producto, id) in $store.state.productos"
         :key="id"
       >
-      <v-col cols="12">
-      <div class="d-flex justify-space-between">
-        <div>
-          
-          <img :src="producto.imagen" alt="" class="img-producto img-fluid" />
-        </div>
-        <div class="text-left pt-4 pr-6">
-          <h4 v-text="producto.nombre"></h4>
-          <p v-text="producto.personas"></p>
-        </div>
-        <div>
-          <h4 class="pt-7 font-weight-bold">$ {{producto.precio.toLocaleString('de-DE', {minimumFractionDigits: 0})}}</h4>
-          <button class="btn-menos mr-1"
-              @click="$store.dispatch('restarCantidadAlProductoDelCarritoDeCompras' , id)" :disabled="producto.qty === 1"
-            >-</button>
-            <span class="px-1"><b>{{producto.qty}}</b></span>
-            <button class="btn-mas"
-              @click="$store.dispatch('agregarCantidadAlProductoDelCarritoDeCompras' , id)"
-            >+</button>
-          <div class="pt-3">
-            <v-btn small plain class="" @click="eliminarProducto(id)">
-              <v-icon color="#c59206">mdi-delete</v-icon>
-            </v-btn>
+        <v-col cols="12">
+          <div class="d-flex justify-space-between">
+            <div>
+              <img
+                :src="producto.imagen"
+                alt=""
+                class="img-producto img-fluid"
+              />
+            </div>
+            <div class="text-left pt-4 pr-6">
+              <h4 v-text="producto.nombre"></h4>
+              <p v-text="producto.personas"></p>
+            </div>
+            <div>
+              <h4 class="pt-7 font-weight-bold">
+                $
+                {{
+                  producto.precio.toLocaleString("de-DE", {
+                    minimumFractionDigits: 0,
+                  })
+                }}
+              </h4>
+              <button
+                class="btn-menos mr-1"
+                @click="
+                  $store.dispatch(
+                    'restarCantidadAlProductoDelCarritoDeCompras',
+                    id
+                  )
+                "
+                :disabled="producto.qty === 1"
+              >
+                -
+              </button>
+              <span class="px-1"
+                ><b>{{ producto.qty }}</b></span
+              >
+              <button
+                class="btn-mas"
+                @click="
+                  $store.dispatch(
+                    'agregarCantidadAlProductoDelCarritoDeCompras',
+                    id
+                  )
+                "
+              >
+                +
+              </button>
+              <div class="pt-3">
+                <v-btn small plain class="" @click="eliminarProducto(id)">
+                  <v-icon color="#c59206">mdi-delete</v-icon>
+                </v-btn>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <hr>
-    </v-col>
-    </v-row>
+          <hr />
+        </v-col>
+      </v-row>
     </v-container>
 
     <!--Container Total-->
@@ -118,14 +175,26 @@
             <h3 class="mb-6">Total del carrito</h3>
             <div class="d-flex justify-space-between">
               <h4>Subtotal</h4>
-              <h4 v-text="`$ ${$store.getters['sumaTotalProductos'].toLocaleString('de-DE', {minimumFractionDigits: 0})}`"></h4>
+              <h4
+                v-text="
+                  `$ ${$store.getters['sumaTotalProductos'].toLocaleString(
+                    'de-DE',
+                    { minimumFractionDigits: 0 }
+                  )}`
+                "
+              ></h4>
             </div>
             <hr />
             <div class="d-flex justify-space-between mt-6 mb-4">
               <h4><b>Total</b></h4>
               <h4
                 class="font-weight-bold"
-                v-text="`$ ${$store.getters['sumaTotalProductos'].toLocaleString('de-DE', {minimumFractionDigits: 0})}`"
+                v-text="
+                  `$ ${$store.getters['sumaTotalProductos'].toLocaleString(
+                    'de-DE',
+                    { minimumFractionDigits: 0 }
+                  )}`
+                "
               ></h4>
             </div>
             <v-btn
@@ -141,7 +210,7 @@
           </div>
         </v-col>
         <!--Col sugerencias-->
-          <SugCarrito />
+        <SugCarrito />
         <!--Col sugerencias-->
       </v-row>
     </v-container>
@@ -151,10 +220,10 @@
 <script>
 import Footer from "../components/Footer";
 import SugCarrito from "../components/SugerenciasCarrito";
-import CryptoJS from 'crypto-js'
-import axios from 'axios'
-import Store from '@/store'
-import querystring from 'query-string'
+import CryptoJS from "crypto-js";
+import axios from "axios";
+import Store from "@/store";
+import querystring from "query-string";
 
 export default {
   name: "Carrito",
@@ -162,18 +231,18 @@ export default {
   data: () => ({
     signature: "",
   }),
-  
+
   async beforeRouteEnter(to, from, next) {
-    await Store.dispatch('getSugerencias' )
-    next()
+    await Store.dispatch("getSugerencias");
+    next();
   },
 
   methods: {
     eliminarProducto(index) {
-      this.$store.state.productos.splice(index, 1); 
+      this.$store.state.productos.splice(index, 1);
     },
     montoApago() {
-      return this.$store.getters['sumaTotalProductos']
+      return this.$store.getters["sumaTotalProductos"];
       // let valorTotal = 0;
       // this.$store.state.productos.forEach((producto) => {
       //   console.log(typeof producto.id);
@@ -184,45 +253,44 @@ export default {
       // return valorTotal;
     },
     crearSignature(form) {
-      const secretKey = '1b48ec1ca7efa74509196577aa3c4ecb0f7fdf11'
-      let signature = '';
+      const secretKey = process.env.VUE_APP_FLOW_SECRET_KEY;
+      let signature = "";
       Object.keys(form).forEach((key) => {
-        signature += `${key}${form[key]}`
-      })
+        signature += `${key}${form[key]}`;
+      });
       console.log(signature);
       let hash = CryptoJS.HmacSHA256(signature, secretKey);
-      console.log('hash',hash.toString());
-      return hash.toString()
+      console.log("hash", hash.toString());
+      return hash.toString();
     },
-    crearOrden(form){
-      axios.post('https://sockets-ceto1.herokuapp.com/payments', form)
-      .then(resp => {
-        console.log('respuestaaa',resp.data.url);
-        setTimeout( () =>  {window.open(resp.data.url)}, 1000 );
-        //redireccionar a form.data.url
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-
+    crearOrden(form) {
+      axios
+        .post("https://sockets-ceto1.herokuapp.com/payments", form)
+        .then((resp) => {
+          console.log("respuestaaa", resp.data.url);
+          setTimeout(() => {
+            window.open(resp.data.url);
+          }, 1000);
+          //redireccionar a form.data.url
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     cobrar() {
-      const order = Math.round(Math.random()*100)
-      const urlConfirmation = 'http://flowosccomerce.tuxpan.com/csepulveda/api2/pay/confirmPay.php' 
-      const urlReturn = 'http://flowosccomerce.tuxpan.com/csepulveda/api2/pay/resultPay.php'
+      const order = Math.round(Math.random() * 100);
       const form = {
         amount: this.montoApago(),
-        apiKey: "53305F1C-A536-4332-9718-7C31DCEL94C8",
+        apiKey: process.env.VUE_APP_FLOW_API_KEY,
         commerceOrder: order,
         email: "vriquelmefe@gmail.com",
         subject: `Compra orden numero: ${order}`,
-        urlConfirmation: urlConfirmation,
-        urlReturn: urlReturn,
+        urlConfirmation: process.env.VUE_APP_FLOW_URL_CONFIRMATION,
+        urlReturn: process.env.VUE_APP_FLOW_URL_RETURN,
       };
-      form.s = this.crearSignature(form)
-      this.crearOrden(form)
+      form.s = this.crearSignature(form);
+      this.crearOrden(form);
     },
-
   },
 };
 </script>
@@ -263,14 +331,16 @@ p {
   font-family: "Montserrat", sans-serif;
   font-weight: 400;
 }
-.btn-menos, .btn-mas {
+.btn-menos,
+.btn-mas {
   color: #262626;
   font-weight: 400;
   box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.15);
   transition: all 0.4s ease;
   border: 1px solid #dddddd;
 }
-.btn-menos:hover, .btn-mas:hover {
+.btn-menos:hover,
+.btn-mas:hover {
   background-color: #ebe9e9;
 }
 .btn-menos {
